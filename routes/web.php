87f534f2f316ca43a0b98c8c178ac8533aa75c33
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2024-10-28 22:26:26 +0630

    view

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Sale;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'sales' => Sale::all(),
+    ]);
 });
 
 Route::get('/dashboard', function () {
